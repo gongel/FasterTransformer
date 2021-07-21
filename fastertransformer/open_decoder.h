901bcdb0c54c031917ac7df2e398eb28dcd0cfd9
gongel <gongel@qq.com>
2021-07-21 19:28:35 +0800

    fix: change max_seq_len_ to 1

--- fastertransformer/open_decoder.h
+++ fastertransformer/open_decoder.h
@@ -245,7 +245,7 @@ namespace fastertransformer
             {
                 /* masked multi-head attention */
                 /* layernorm(from_tensor) -> norm_from_tensor_buf_ */
-                print_tensor(batch_size_*max_seq_len_*head_num_*size_per_head_,from_tensor,"from_tensor.txt");
+                print_tensor(batch_size_*1*head_num_*size_per_head_,from_tensor,"from_tensor.txt");
 
                 decoder_norm1(from_tensor,
                               param_.self_layernorm.gamma,
@@ -253,7 +253,7 @@ namespace fastertransformer
                               norm_from_tensor_buf_,
                               m,
                               n);
-                print_tensor(batch_size_*max_seq_len_*head_num_*size_per_head_,norm_from_tensor_buf_,"from_tensor_after_decoder_norm1.txt");
+                print_tensor(batch_size_*1*head_num_*size_per_head_,norm_from_tensor_buf_,"from_tensor_after_decoder_norm1.txt");
 
 #ifndef NDEBUG
                 cudaDeviceSynchronize();
